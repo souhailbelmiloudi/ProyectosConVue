c2dee6ce0213713bdf46ec0fd97d5c9b74c4ca7b
souhail belmiloudi <souhailbelmiloudi@gmail.com>
2024-01-10 21:09:57 +0100

    Updates

---
+++ App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ElJuego from './components/ElJuego.vue'
+
+</script>
+
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen bg-gray-800">
+    <ElJuego />
+  </div>
+  
+
+  
+</template>
+
+
+
+
+
